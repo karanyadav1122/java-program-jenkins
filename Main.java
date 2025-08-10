c433dee9c2086486743208ab5638dbaaa51ccaaf
Karan Yadav <karanyadav1122@gmail.com>
2025-08-10 16:10:40 -0500

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("lucky kabootar");
+    System.out.println("hello how are you");
+    System.out.println(new Date());
+  }
+}
